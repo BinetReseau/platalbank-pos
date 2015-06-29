5bfe7d8c2ea3657631e76e4d11ad95a5b825cc1f
Nadrieril <nadrieril@gmail.com>
2015-06-29 15:56:46 +0200

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name platalbankPosApp
+ * @description
+ * # platalbankPosApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('platalbankPosApp', [
+    'ngResource'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name platalbankPosApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the platalbankPosApp
+ */
+angular.module('platalbankPosApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
